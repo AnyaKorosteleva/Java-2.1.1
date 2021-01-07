f3de07e91e329ae1bea1d73607157fa6afb8d9f7
AnyaKorosteleva <74379643+AnyaKorosteleva@users.noreply.github.com>
2021-01-07 20:44:13 +0300

    formatting rules

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 500;
+        int ticketPrice = 500;
         int miles = 20;
-        int bonus = TicketPrice/miles;
+        int bonus = ticketPrice / miles;
         System.out.println(bonus);
     }
 }

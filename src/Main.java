874d2f89c65baf25c34e1c2e50f90c75367fca44
AnyaKorosteleva <74379643+AnyaKorosteleva@users.noreply.github.com>
2021-01-07 19:54:50 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 500;
+        int miles = 20;
+        int bonus = TicketPrice/miles;
+        System.out.println(bonus);
+    }
+}
